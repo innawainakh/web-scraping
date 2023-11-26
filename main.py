28edf3660f003d3e200156c8e4a58d3805df6344
innawainakh <innashishani1@gmail.com>
2023-11-26 21:24:23 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+user_input = input("enter country:  ")
+def weather1():
+    weather_url = "https://www.timeanddate.com/weather/"
+    weather = weather_url + user_input
+    weather_page = requests.get(weather)
+    content = weather_page.content
+    soup = BeautifulSoup(content, "lxml")
+    web1 = soup.findAll("div", {"class": "bk-focus__info"})
+    weather_data_web1=[]
+    for i in web1:
+        country=i.contents[0].find("tbody")
+        rows = country.find_all("tr")
+        for row in rows:
+            data = ' '.join(r.get_text(strip=True) for r in row.find_all(["th", "td"]))
+            weather_data_web1.append(data)
+            weather_data_text1 = '\n'.join(weather_data_web1)
+    return weather_data_text1
+def weather2():
+    weather_url = "https://www.timeanddate.com/weather/"
+    weather = weather_url + user_input
+    weather_page = requests.get(weather)
+    content = weather_page.content
+    soup = BeautifulSoup(content, "lxml")
+    web2=soup.findAll("div", {"class": "bk-focus__qlook"})
+    weather_data_web2=[]
+    for x in web2:
+        country_capital=x.find("a")
+        weather= x.find("div", class_="h2")
+        paragraphs = x.find_all("p")
+        print("Country Capital:", country_capital.text.strip())
+        print("Weather:", weather.text.strip())
+        for p in paragraphs:
+           data=p.get_text("\n", strip=True)
+           weather_data_web2.append(data)
+           weather_data_text2 = '\n'.join(weather_data_web2)
+    return weather_data_text2
+def time_zone():
+    weather_url = "https://www.timeanddate.com/time/zone/"
+    weather = weather_url + user_input
+    weather_page = requests.get(weather)
+    content = weather_page.content
+    soup = BeautifulSoup(content, "lxml")
+    web1 = soup.findAll("div", {"class": "bk-focus__info"})
+    time_zone_data=[]
+    for i in web1:
+        country = i.contents[0].find("tbody")
+        rows = country.find_all("tr")
+        for row in rows:
+            data = ' '.join(r.get_text(strip=True) for r in row.find_all(["th", "td"]))
+            time_zone_data.append(data)
+            time_data_text = '\n'.join(time_zone_data)
+    print("-----------------")
+    return time_data_text
+weather_data_text1 = weather1()
+print(weather_data_text1)
+print("-------------")
+weather_data_text2=weather2()
+print(weather_data_text2)
+time_data_text = time_zone()
+print(time_data_text)
+with open(f"{user_input.lower()}.csv", "a",encoding="utf-8")as file:
+ csv_writer=csv.writer(file)
+ csv_writer.writerow(["data ", "value" ])
+ csv_writer.writerow([weather_data_text1])
+ csv_writer.writerow([weather_data_text2])
+ csv_writer.writerow([time_data_text])
